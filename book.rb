78dcf39e7f6acd93e70f4871cdfda22893ef7d8f
yosaddis <email.to.yoseph@gmail.com>
2023-06-24 23:15:54 +0300

    Add book class

---
+++ book.rb
@@ -0,0 +1,18 @@
+require_relative 'item'
+
+class Book < Item
+  attr_accessor :title, :cover_state
+  attr_reader :publisher
+
+  def initialize(title, publisher, cover_state, *args)
+    super(*args)
+    @publisher = publisher
+    @title = title
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super || @cover_state.downcase == 'bad'
+  end
+end
