9fa23ec097e5240d5eceff1f73032f5c0a3d37f2
cosmosha <cosmoshagan@gmail.com>
2023-06-24 19:43:04 +0000

    Add item class methods

--- item.rb
+++ item.rb
@@ -10,4 +10,29 @@ class Item
     @archived = archived
   end
 
+  def can_be_archived?
+    Date.today.year - Date.parse(@publish_date).year > 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+
+  def add_label=(label)
+    @label = label
+    label.add_item(self)
+  end
+
+  def add_genre=(genre)
+    @genre = genre
+    genre.add_item(self)
+  end
+
+  def author=(author)
+    @author = author
+    author.items.push(self) unless author.items.include?(self)
+  end
+  
+  private :can_be_archived?
+
 end
