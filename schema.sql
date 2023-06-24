18f004e4348697f1c0091ac7800b4b519c27efe8
cosmosha <cosmoshagan@gmail.com>
2023-06-24 21:31:14 +0000

    Update schema.sql with game and author tables

--- schema.sql
+++ schema.sql
@@ -28,3 +28,21 @@ CREATE TABLE Genres(
 );
 
 alter table music_albums add constraint fk_genre_id foreign key (genre_id) references genres(id);
+
+CREATE TABLE game (
+  id SERIAL PRIMARY KEY,
+  multiplayer VARCHAR(100) NOT NULL,
+  last_played_at DATE NOT NULL,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL DEFAULT false,
+  genre_id INT REFERENCES genre (id),
+  author_id INT REFERENCES author (id),
+  source_id INT REFERENCES source (id),
+  label_id INT REFERENCES label (id)
+);
+
+CREATE TABLE author (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+)
